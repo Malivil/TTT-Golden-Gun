413336ad3e657b52e0b99903efda17908839a785
Malivil <mlebson@outlook.com>
2020-10-15 15:39:16 -0400

    Moved distribution scripts to their own repo

--- UpdateGoldenGun.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p changes="Enter Changes: "
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe"  update -addon "TTT-Golden-Gun.gma" -id "2055695161" -changes "%changes%"
-pause
