c98eafb872bb641a2db29b76b9c7057e11c6a5bb
Malivil <mlebson@outlook.com>
2021-07-15 08:28:29 -0400

    Added unique logic for Bodysnatcher and Beggar

--- lua/weapons/weapon_ttt_nrgoldengun/shared.lua
+++ lua/weapons/weapon_ttt_nrgoldengun/shared.lua
@@ -15,8 +15,10 @@ if SERVER then
 	resource.AddFile("materials/models/weapons/v_models/feets/v_hands.vtf")
     resource.AddFile("materials/models/weapons/v_models/feets/v_hands_normal.vtf")
 
+    util.AddNetworkString("TTT_RoleChanged")
     util.AddNetworkString("TTT_Zombified")
     util.AddNetworkString("TTT_DrunkSober")
+    util.AddNetworkString("TTT_ScoreBodysnatch")
 end
 
 if CLIENT then
@@ -109,28 +111,37 @@ local function IsIndependentTeam(ply)
     return ply.IsIndependentTeam and ply:IsIndependentTeam()
 end
 
+function SetRole(ply, role)
+    ply:SetRole(role)
+
+    net.Start("TTT_RoleChanged")
+    net.WriteString(ply:SteamID64())
+    net.WriteUInt(role, 8)
+    net.Broadcast()
+end
+
 function SWEP:PrimaryAttack()
     if (not self:CanPrimaryAttack()) then return end
     self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
     self.Weapon:SendWeaponAnim(ACT_VM_PRIMARYATTACK)
     self:TakePrimaryAmmo(1)
-    self.Owner:EmitSound(Sound("Weapon_Deagle.Single"))
+    self:GetOwner():EmitSound(Sound("Weapon_Deagle.Single"))
 
-    local trace = util.GetPlayerTrace(self.Owner)
+    local trace = util.GetPlayerTrace(self:GetOwner())
     local tr = util.TraceLine(trace)
     if tr.Entity.IsPlayer() then
         local ply = tr.Entity
         -- Set the owner on fire for 5 seconds
         if ply:GetRole() == ROLE_PHANTOM then
-            if SERVER then self.Owner:Ignite(5) end
+            if SERVER then self:GetOwner():Ignite(5) end
         -- Reduce the health of both the Owner and the Target by the configured amount
         elseif ply:GetRole() == ROLE_KILLER then
             if SERVER then
                 local killerdamage = GetConVar("ttt_gdeagle_killer_damage"):GetInt()
-                if self.Owner:Health() > killerdamage then
-                    self.Owner:SetHealth(self.Owner:Health() - killerdamage)
+                if self:GetOwner():Health() > killerdamage then
+                    self:GetOwner():SetHealth(self:GetOwner():Health() - killerdamage)
                 else
-                    self.Owner:Kill()
+                    self:GetOwner():Kill()
                 end
                 if ply:Health() > killerdamage then
                     ply:SetHealth(ply:Health() - killerdamage)
@@ -142,15 +153,15 @@ function SWEP:PrimaryAttack()
         elseif ply:GetRole() == ROLE_ZOMBIE then
             if SERVER then
                 net.Start("TTT_Zombified")
-                net.WriteString(self.Owner:Nick())
+                net.WriteString(self:GetOwner():Nick())
                 net.Broadcast()
 
-                self.Owner:SetRole(ROLE_ZOMBIE)
-                if self.Owner.SetZombiePrime then
-                    self.Owner:SetZombiePrime(false)
+                self:GetOwner():SetRole(ROLE_ZOMBIE)
+                if self:GetOwner().SetZombiePrime then
+                    self:GetOwner():SetZombiePrime(false)
                 end
-                self.Owner:StripWeapons()
-                self.Owner:Give("weapon_zom_claws")
+                self:GetOwner():StripWeapons()
+                self:GetOwner():Give("weapon_zom_claws")
                 SendFullStateUpdate()
             end
         -- Turn the owner into a pile of bones and heal the target
@@ -159,9 +170,9 @@ function SWEP:PrimaryAttack()
                 local vamheal = GetConVar("ttt_gdeagle_vampire_heal"):GetInt()
                 local vamoverheal = GetConVar("ttt_gdeagle_vampire_overheal"):GetInt()
                 ply:SetHealth(math.min(ply:Health() + vamheal, ply:GetMaxHealth() + vamoverheal))
-                self:DropBones(self.Owner)
-                local sid = self.Owner:SteamID()
-                self.Owner:Kill()
+                self:DropBones(self:GetOwner())
+                local sid = self:GetOwner():SteamID()
+                self:GetOwner():Kill()
                 RemoveRagdoll(sid)
             end
         -- Have the drunk immediately remember their role
@@ -186,28 +197,66 @@ function SWEP:PrimaryAttack()
 
                 SendFullStateUpdate()
             end
+        -- Switch roles with the bodysnatcher
+        elseif ply:GetRole() == ROLE_BODYSNATCHER then
+            if SERVER then
+                local owner = self:GetOwner()
+                net.Start("TTT_ScoreBodysnatch")
+                net.WriteString(owner:Nick())
+                net.WriteString(ply:Nick())
+                net.WriteString(ROLE_STRINGS_EXT[owner:GetRole()])
+                net.Broadcast()
+
+                local victim_role = ply:GetRole()
+                local attacker_role = owner:GetRole()
+                owner:SetRole(victim_role)
+                ply:SetRole(attacker_role)
+
+                ply:StripWeapon("weapon_bod_bodysnatch")
+                owner:StripWeapon("weapon_ttt_wtester")
+
+                owner:Give("weapon_bod_bodysnatch")
+                ply:Give("weapon_ttt_wtester")
+
+                SendFullStateUpdate()
+            end
+        -- Change beggars to be the opposite team of the player who shot them (or a random team if shot by a Jester or Independent)
+        elseif ply:GetRole() == ROLE_BEGGAR then
+            if SERVER then
+                local target_role
+                if IsTraitorTeam(self:GetOwner()) then
+                    target_role = ROLE_INNOCENT
+                elseif IsInnocentTeam(self:GetOwner()) then
+                    target_role = ROLE_TRAITOR
+                else
+                    target_role = math.random(0, 1) == 1 and ROLE_INNOCENT or ROLE_TRAITOR
+                end
+                SetRole(ply, target_role)
+
+                SendFullStateUpdate()
+            end
         -- Kill traitors outright
         elseif IsTraitorTeam(ply) then
             local bullet = {}
-            bullet.Attacker = self.Owner
+            bullet.Attacker = self:GetOwner()
             bullet.Num = self.Primary.NumberofShots
-            bullet.Src = self.Owner:GetShootPos()
-            bullet.Dir = self.Owner:GetAimVector()
+            bullet.Src = self:GetOwner():GetShootPos()
+            bullet.Dir = self:GetOwner():GetAimVector()
             bullet.Spread = Vector(0, 0, 0)
             bullet.Tracer = 0
             bullet.Force = 3000
             bullet.Damage = 4000
             bullet.AmmoType = self.Primary.Ammo
-            self.Owner:FireBullets(bullet)
+            self:GetOwner():FireBullets(bullet)
         -- Kill the owner if the target was innocent
         elseif IsInnocentTeam(ply) then
-            if SERVER then self.Owner:Kill() end
+            if SERVER then self:GetOwner():Kill() end
         -- Set the shooter's health to the target's health, if it's less than 100
         -- Then restore the target's max health to at least 100 and fully heal them
         elseif IsIndependentTeam(ply) then
             local hp = ply:Health()
-            if hp < 100 and hp < self.Owner:Health() then
-                self.Owner:SetHealth(hp)
+            if hp < 100 and hp < self:GetOwner():Health() then
+                self:GetOwner():SetHealth(hp)
             end
 
             local max = ply:GetMaxHealth()
@@ -219,7 +268,7 @@ function SWEP:PrimaryAttack()
         -- Kill the jester team and the shooter
         elseif IsJesterTeam(ply) then
             if SERVER then
-                self.Owner:Kill()
+                self:GetOwner():Kill()
                 ply:SetHealth(0)
                 ply:Kill()
             end
